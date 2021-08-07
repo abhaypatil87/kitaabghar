358eba6b9190cc502b7b26e9094001f8183d733b
Abhay Patil <abhaypatil87@gmail.com>
2021-08-06 22:08:06 -0600

    Added favicon

--- src/Views/layouts/LoginLayout.tsx
+++ src/Views/layouts/LoginLayout.tsx
@@ -1,5 +1,6 @@
 import { Link as RouterLink, Outlet } from "react-router-dom";
 import { experimentalStyled as styled } from "@material-ui/core/styles";
+import Logo from "../../components/Sidebar/Logo";
 
 const StyledHeader = styled("header")(({ theme }) => ({
   top: 0,
@@ -16,8 +17,8 @@ const LoginLayout = () => {
   return (
     <>
       <StyledHeader>
-        <RouterLink to="/" style={{ textDecoration: "none" }}>
-          {"क़िताबघर / Kitaabghar"}
+        <RouterLink to="/" style={{ textDecoration: "none", color: "inherit" }}>
+          <Logo />
         </RouterLink>
       </StyledHeader>
       <Outlet />
